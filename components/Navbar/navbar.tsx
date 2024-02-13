74831c8397e8e5868ebcb933620304fb0ddde138
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-02-13 15:03:50 +0530

    added shadcn and dark theme

---
+++ components/Navbar/navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from '../ui/button'
+
+function Navbar() {
+  return (
+    <div>
+      <Button variant={'outline'}> 
+        Hello World!
+      </Button>
+    </div>
+  )
+}
+
+export default Navbar
