132bb50aabd19116b9d145a01dffe53186f87152
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-02-13 15:56:03 +0530

    nav toggle and style

--- components/Navbar/navbar.tsx
+++ components/Navbar/navbar.tsx
@@ -1,18 +1,30 @@
+"use client"
 import React from 'react'
 import Link from 'next/link'
+import { useState } from 'react'
 
 function Navbar() {
-  return (
-    <div>
-        {["Home", "About", "Guests", "Sponsors", "Team", "Events" , "Contact"].map((item, index) => {
-            return(
-                <Link key={index} href={index !== 0 ? `/${item.toLowerCase()}` : '/'}>
-                    {item}
-                </Link>
-            )
-        })}
-    </div>
-  )
+
+    const [navToggle, setNavToggle] = useState(false);
+
+    return (
+        <div className='flex justify-between items-center w-full px-16 py-2'>
+            <h1>
+                HORIZON 24'
+            </h1>
+            <div className='flex gap-8 text-lg'>
+                {navToggle ? (
+                    ["Home", "About", "Guests", "Sponsors", "Team", "Events", "Contact"].map((item, index) => (
+                        <Link key={index} href={index !== 0 ? `/${item.toLowerCase()}` : '/'}>
+                            {item}
+                        </Link>
+                    ))
+                ) : (
+                    <div onClick={() => setNavToggle(true)}>Menu</div>
+                )}
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
