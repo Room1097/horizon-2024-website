b7a3b29fe9420c586652aa7b2b7e40ca5649730c
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-02-18 03:26:15 +0530

    added glass to navbar

--- components/Navbar/navbar.tsx
+++ components/Navbar/navbar.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React, { useState, useRef, useEffect } from 'react';
 import FullLengthNavbar from './fullLengthNavbar';
+import './navbar.css'
 
 function Navbar() {
     const [navToggle, setNavToggle] = useState(false);
@@ -19,7 +20,7 @@ function Navbar() {
     }, []);
 
     return (
-        <div className='flex justify-between items-start w-full lg:px-16  lg:py-2 fixed z-30 '>
+        <div className='glass flex justify-between items-start w-full lg:px-16  lg:py-2 fixed z-30 '>
             <h1 className='p-2'>HORIZON</h1>
             <div className='flex gap-8 text-lg' ref={Nav}>
                 {navToggle ? (
