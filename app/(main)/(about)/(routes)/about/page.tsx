072ba90d972a269daa1379c10f77c69617f50633
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-02-18 16:58:32 +0530

    responsive about us page

--- app/(main)/(about)/(routes)/about/page.tsx
+++ app/(main)/(about)/(routes)/about/page.tsx
@@ -12,7 +12,6 @@ const About = () => {
       title: "Hands On Experience",
       description: "",
     },
-
     {
       title: "Guest Lectures",
       description: "",
@@ -22,8 +21,8 @@ const About = () => {
   return (
     <div className="w-full pt-20 min-h-screen relative">
       <div className="relative">
-        <div style={{ width: "100%", height: "100vh" }}>
-          <div className="absolute inset-0 bg-black opacity-30 z-10"></div>
+        <div className="lg:w-full lg:h-screen w-[90%] h-[55vh]">
+          <div className="absolute inset-0 bg-black opacity-40 z-10"></div>
           <Image
             src="/images/HOME.svg"
             alt="campus-image"
@@ -33,17 +32,19 @@ const About = () => {
           />
         </div>
         {/* Text */}
-        <div className="absolute top-[20%] left-1/2 transform -translate-x-1/2 -translate-y-1/4 text-zinc-100 text-center z-20">
-          <h1 className="text-4xl tracking-wide font-mono font-bold">
+        <div className="top-[15%] w-[80%] left-1/2 absolute lg:top-[20%] lg:left-1/2 transform -translate-x-1/2 -translate-y-1/4 text-zinc-100 text-center z-20">
+          <h1 className="lg:text-4xl text-xl tracking-wide font-mono font-bold">
             Indian Institute of Information Technology Vadodara
           </h1>
-          <h3 className="text-2xl tracking-wide font-mono font-bold">International Campus Diu</h3>
+          <h3 className="lg:text-2xl tracking-wide font-mono font-bold">
+            International Campus Diu
+          </h3>
         </div>
       </div>
 
-      <div className="text-center mt-16">
+      <div className="text-center mt-8 lg:mt-16">
         <h1 className="text-4xl tracking-wide font-mono font-bold">About Us</h1>
-        <div className="text-lg font-mono mt-16 text-zinc-200 flex flex-col gap-8 items-center px-16">
+        <div className="text-left pl-12 pr-4 lg:text-center text-sm lg:text-lg font-mono mt-8 lg:mt-16 text-zinc-200 flex flex-col gap-8 items-center lg:px-16">
           <p>
             Join us for an incredible experience at{" "}
             <span className="font-primary tracking-widest">HORIZON</span>, the
@@ -66,16 +67,14 @@ const About = () => {
         </div>
       </div>
 
-      <div className="w-full flex flex-col justify-center items-center">
-        <div className="flex justify-around w-screen my-28">
-          {joinUsData.map((workshop, index) => (
-            <JoinUsCard
-              key={index}
-              title={workshop.title}
-              description={workshop.description}
-            />
-          ))}
-        </div>
+      <div className="flex flex-col lg:flex-row items-center justify-center gap-5 w-screen lg:px-20 px-4 lg:my-28 my-16">
+        {joinUsData.map((workshop, index) => (
+          <JoinUsCard
+            key={index}
+            title={workshop.title}
+            description={workshop.description}
+          />
+        ))}
       </div>
     </div>
   );
