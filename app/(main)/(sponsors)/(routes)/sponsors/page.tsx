aeda57f1108ff1af30b39d8e164ca4529ba18335
Trijay1709 <trijaypatel1709@gmail.com>
2024-02-18 16:26:41 +0530

    Sponsor card design

--- app/(main)/(sponsors)/(routes)/sponsors/page.tsx
+++ app/(main)/(sponsors)/(routes)/sponsors/page.tsx
@@ -1,9 +1,16 @@
+import SponsorCard from '@/components/Sponsor Card/sponsorCard'
 import React from 'react'
 
 const Sponsors = () => {
   return (
-    <div>
-      Sponsor Page
+    <div className='pt-20 w-screen'>
+      <h1 className='text-4xl font-primary w-screen text-center'>Sponsors</h1>
+      <div className='flex flex-wrap gap-6 justify-center'>
+      <SponsorCard img='https://logowik.com/content/uploads/images/wolfram-corporate6412.logowik.com.webp' title='Title' name='Wolfram'/>
+      <SponsorCard img='https://logowik.com/content/uploads/images/wolfram-corporate6412.logowik.com.webp' title='Ecosystem' name='Wolfram'/>
+      <SponsorCard img='https://logowik.com/content/uploads/images/wolfram-corporate6412.logowik.com.webp' title='Logistics' name='Wolfram'/>
+      <SponsorCard img='https://logowik.com/content/uploads/images/wolfram-corporate6412.logowik.com.webp' title='Title' name='Wolfram'/>
+      </div>
     </div>
   )
 }
