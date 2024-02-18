ec1dea23fa6f437d7322607a8116424989a7d69a
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-02-19 03:22:33 +0530

    completed sponsor card

--- app/(main)/(sponsors)/(routes)/sponsors/page.tsx
+++ app/(main)/(sponsors)/(routes)/sponsors/page.tsx
@@ -5,8 +5,9 @@ const Sponsors = () => {
   return (
     <div className='pt-20 w-full'>
       <h1 className='text-4xl font-mono w-full pb-7 text-center'>Sponsors</h1>
-      <div className='flex flex-wrap gap-y-10 gap-x-10 justify-evenly  lg:ml-0 '>
-        <SponsorCard img='https://logowik.com/content/uploads/images/wolfram-corporate6412.logowik.com.webp' title='Title' name='Wolfram'/>
+      <div className='flex flex-wrap lg:flex-row flex-col gap-y-10 gap-x-10 justify-evenly  lg:ml-0 '>
+        <SponsorCard img='/images/SponsorLogos/Azure.svg' title='Ecosystem' subtitle='Partner' name='Wolfram'/>
+        <SponsorCard img='/images/SponsorLogos/Reskilll.svg' title='Ecosystem' subtitle='Partner' name='Wolfram'/>
       </div>
     </div>
   )
--- components/SponsorCard/sponsorCard.tsx
+++ components/SponsorCard/sponsorCard.tsx
@@ -1,20 +1,41 @@
+"use client";
 import React from "react";
-import "./sponsor.css";
 import BlurImage from "../BlurImage/blurimage";
-import { Separator } from "@/components/ui/separator";
+import Image from "next/image";
 
 interface SponsorCardProps {
   img: string;
   name: string;
   title: string;
+  subtitle: string;
 }
 
 const SponsorCard: React.FC<SponsorCardProps> = (props) => {
-  const { img, name, title } = props;
+  const { img, name, title, subtitle } = props;
 
   return (
-    <div className="main-sponsor-container w-[20rem] h-[20rem]">
-        {/* <BlurImage /> */}
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="font-mono text-xl">{title}</h1>
+      <h1 className="font-mono text-xl">{subtitle}</h1>
+      <div className="main-sponsor-box w-[20rem] h-[20rem] relative">
+      <div className="absolute inset-0">
+        <BlurImage
+          image="/images/SponsorCard/sponsor-bg.svg"
+          width={400}
+          height={400}
+          alt="missing"
+          rounded="rounded-xl"
+        />
+      </div>
+      <div className="w-[16rem] absolute top-[30%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
+        <Image
+          src={img}
+          width={800}
+          height={800}
+          alt="missing"
+        />
+      </div>
+    </div>
     </div>
   );
 };
