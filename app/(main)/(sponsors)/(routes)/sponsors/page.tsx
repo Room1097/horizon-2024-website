13d4f9dbd128b09f6a64159e0362c07eed7a8701
Trijay1709 <trijaypatel1709@gmail.com>
2024-02-18 23:00:21 +0530

    sponsor bug fix

--- app/(main)/(sponsors)/(routes)/sponsors/page.tsx
+++ app/(main)/(sponsors)/(routes)/sponsors/page.tsx
@@ -5,11 +5,13 @@ const Sponsors = () => {
   return (
     <div className='pt-20 w-full'>
       <h1 className='text-4xl font-primary bordear-2 w-full pb-7 text-center'>Sponsors</h1>
-      <div className='flex flex-wrap gap-y-6 justify-center  lg:ml-0 '>
+      <div className='flex flex-wrap gap-y-10 gap-x-10 justify-evenly  lg:ml-0 '>
       <SponsorCard img='https://logowik.com/content/uploads/images/wolfram-corporate6412.logowik.com.webp' title='Title' name='Wolfram'/>
       <SponsorCard img='https://logowik.com/content/uploads/images/wolfram-corporate6412.logowik.com.webp' title='Ecosystem' name='Wolfram'/>
       <SponsorCard img='https://logowik.com/content/uploads/images/wolfram-corporate6412.logowik.com.webp' title='Logistics' name='Wolfram'/>
       <SponsorCard img='https://logowik.com/content/uploads/images/wolfram-corporate6412.logowik.com.webp' title='Title' name='Wolfram'/>
+      <SponsorCard img='https://logowik.com/content/uploads/images/wolfram-corporate6412.logowik.com.webp' title='Title' name='Wolfram'/>
+      <SponsorCard img='https://logowik.com/content/uploads/images/wolfram-corporate6412.logowik.com.webp' title='Media' name='Wolfram'/>
       </div>
     </div>
   )
