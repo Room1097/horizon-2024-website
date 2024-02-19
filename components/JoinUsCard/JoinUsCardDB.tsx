597ecb0446f1d0208c459cc192a6a09937c003d2
Trijay1709 <trijaypatel1709@gmail.com>
2024-02-20 02:39:42 +0530

    db update

--- components/JoinUsCard/JoinUsCardDB.tsx
+++ components/JoinUsCard/JoinUsCardDB.tsx
@@ -7,11 +7,11 @@ export const joinUsData = [
     {
       title: "Hands On Experience",
       description: "",
-      image : "/images/Symbols/Workshop.png"
+      image : "/images/Symbols/3.svg"
     },
     {
       title: "Guest Lectures",
       description: "",
-      image : "/images/Symbols/Workshop.png"
+      image : "/images/Symbols/4.svg"
     },
   ];
