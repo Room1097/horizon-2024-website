20fd13d1417542b46ccd5dcb6dcc40a6598f8b0a
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-02-23 03:46:58 +0530

    updated key Event DB

--- components/KeyEventCards/KeyEventsCardsDB.tsx
+++ components/KeyEventCards/KeyEventsCardsDB.tsx
@@ -1,7 +1,7 @@
 export const eventDets = [
     {
         title: "ScrapeWeb",
-        poster:"/images/Posters/RoboArena.png",
+        poster:"/images/Posters/ScrapWeb.png",
         desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste dolorem magnam tempora vero quas eaque omnis ducimus? Ut earum reiciendis aperiam laboriosam sequi, voluptatem voluptate in dicta quas repellat laborum obcaecati fugiat corrupti deleniti dolore rem iure, unde tenetur nemo?"
     },
     {
@@ -11,14 +11,20 @@ export const eventDets = [
     },
     {
         title: "Code-A-Bot",
-        poster:"/images/Posters/RoboArena.png",
+        poster:"/images/Posters/HACKATHON.png",
         desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste dolorem magnam tempora vero quas eaque omnis ducimus? Ut earum reiciendis aperiam laboriosam sequi, voluptatem voluptate in dicta quas repellat laborum obcaecati fugiat corrupti deleniti dolore rem iure, unde tenetur nemo?"
     },
     {
+        title: "Code Voyage",
+        poster:"/images/Posters/Coding.png",
+        desc: "Code Voyage by Coding Club DCodr, IIIT Vadodara - International Campus Diu, is a thrilling coding event in Horizon tech fest. With two rounds of challenges, it invites talented coders from multiple colleges to showcase their skills, and intelligence and compete for recognition. Join us for an exciting journey of innovation and excellence!"
+    },
+    {
         title: "Vermithor",
-        poster:"/images/Posters/RoboArena.png",
+        poster:"/images/Posters/Vermithor.png",
         desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste dolorem magnam tempora vero quas eaque omnis ducimus? Ut earum reiciendis aperiam laboriosam sequi, voluptatem voluptate in dicta quas repellat laborum obcaecati fugiat corrupti deleniti dolore rem iure, unde tenetur nemo?"
     },
+    
     {
         title: "Capture Point 5353 2.0",
         poster:"/images/Posters/CTF.png",
--- components/KeyEventCards/keyeventcards.tsx
+++ components/KeyEventCards/keyeventcards.tsx
@@ -12,13 +12,13 @@ const KeyEventCards: React.FC<KeyEventDets> = ({ title, poster, desc, reverse })
   return (
     <div className={`lg:w-1/2 w-[90%] flex ${reverse ? "lg:flex-row-reverse" : "lg:flex-row"} flex-col items-center gap-8 border-2 p-6 rounded-lg border-zinc-600 bg-zinc-800`}>
       <div className="rounded-xl">
-        <BlurImage image={poster} width={190} height={300} alt="poster" />
+        <BlurImage image={poster} width={190} height={300} alt="poster" rounded='rounded-sm'/>
       </div>
-      <div className="w-full gap-8 flex flex-col items-center">
+      <div className={`w-full gap-4 flex flex-col ${reverse ? "text-left" : "text-right"} items-center`}>
         <h1 className="text-2xl font-transformer underline tracking-widest">
           {title}
         </h1>
-        <p className="text-sm text-center">{desc}</p>
+        <p className="text-sm font-mono">{desc}</p>
       </div>
     </div>
   );
