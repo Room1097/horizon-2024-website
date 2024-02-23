a0a742324feed30b62ef039bb7a778d1cff53186
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-02-23 10:17:57 +0530

    bug fix

--- components/EventCard/EventCard.tsx
+++ components/EventCard/EventCard.tsx
@@ -15,9 +15,9 @@ const EventCard: React.FC<EventCardProps> = (props) => {
   const { img, name, eventDate, regDate, organizingClub, accent } = props;
 
   return (
-    <div className="border lg:w-max w-full lg:max-w-[24rem] border-gray-300 rounded-lg p-4 lg:p-[0.75rem] shadow-md flex flex-col">
-      <div className="flex items-center rounded-lg overflow-hidden justify-center w-[75vw] lg:w-[19.6875rem] h-[27.8125rem] lg:h-[28rem] object-cover">
-      <img src={img} alt="" className="w-[20rem] h-[31.25rem] object-contain "/>
+    <div className="border lg:w-max w-[full] mx-2 lg:max-w-[24rem] border-gray-300 rounded-lg lg:p-4 p-6 shadow-md flex flex-col">
+      <div className="flex items-center rounded-xl overflow-hidden justify-center object-cover">
+      <img src={img} alt="" className="object-contain rounded-xl w-[75vw] lg:w-[19.6875rem] h-[27.8125rem] lg:h-[28rem]"/>
       </div>
       <div className="mt-4 font-mono">
         <h2 className="text-xl lg:text-[1.25rem] font-semibold">{name}</h2>
