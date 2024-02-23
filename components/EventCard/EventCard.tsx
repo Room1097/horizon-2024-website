11f893a068dc1464d1dc71ffff02dc3fa5885f92
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-02-23 09:06:56 +0530

    bug fix

--- components/EventCard/EventCard.tsx
+++ components/EventCard/EventCard.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client'
 import React from "react";
 import { Button } from "../ui/button";
 import BlurImage from "../BlurImage/blurimage";
@@ -15,8 +15,8 @@ const EventCard: React.FC<EventCardProps> = (props) => {
   const { img, name, eventDate, regDate, organizingClub, accent } = props;
 
   return (
-    <div className="border lg:w-max w-[80vw] border-gray-300 rounded-lg p-4 shadow-md flex flex-col">
-      <div className="flex items-center justify-center w-[315px] h-[446px] object-cover">
+    <div className="border lg:w-max w-full lg:max-w-[24rem] border-gray-300 rounded-lg p-4 lg:p-[0.75rem] shadow-md flex flex-col">
+      <div className="flex items-center justify-center w-[75vw] lg:w-[19.6875rem] h-[27.8125rem] lg:h-[22rem] object-cover">
         <BlurImage
           alt="poster"
           image={img}
@@ -27,7 +27,7 @@ const EventCard: React.FC<EventCardProps> = (props) => {
         />
       </div>
       <div className="mt-4 font-mono">
-        <h2 className="text-xl font-semibold">{name}</h2>
+        <h2 className="text-xl lg:text-[1.25rem] font-semibold">{name}</h2>
         <p className="text-gray-100">
           <span className="text-zinc-500 underline">Event Date</span> :{" "}
           {eventDate}
